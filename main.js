9308a2c576cfa9d8808717cac694bc9604b106be
Jéssica Nascimento <160242497+profajessica12@users.noreply.github.com>
2024-06-11 08:41:59 -0300

    salvae

--- main.js
+++ main.js
@@ -7,4 +7,6 @@ for(let i=0;i <botoes.length;i++){
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");    }}
-
+        const contadores = document.querySelectorAll(“.contador”);
+        const tempoObjetivo1 = new Date(“2024-10-05”);
+        contadores[0].textContent = tempoObjetivo1;
