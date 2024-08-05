12b6312610cdb60a4135ae62d3a31857a4687447
Jéssica Nascimento <160242497+profajessica12@users.noreply.github.com>
2024-08-05 08:00:00 -0300

    aula 04

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
-        
+
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
         }
-        botoes[i].classList.add("ativo");    }}
-        const contadores = document.querySelectorAll(“.contador”);
-        const tempoObjetivo1 = new Date(“2024-10-05”);
-        contadores[0].textContent = tempoObjetivo1;
+
+        botoes[i].classList.add("ativo");
+    }
+}
